e972950e7b91d0a08e1a0d14aa5b08c8e806f8b0
timstr <timstr@cs.ubc.ca>
2020-11-21 15:27:08 -0800

    Removed obsolete helper elements

--- include/OFC/DOM/BoxElement.hpp
+++ include/OFC/DOM/BoxElement.hpp
@@ -30,18 +30,4 @@ namespace ofc::ui::dom {
         RoundedRectangle m_rect;
     };
 
-    // Convenient template for mixing BoxElement with any container
-    template<typename ContainerType>
-    class Boxed : public ContainerType, public BoxElement {
-    public:
-        static_assert(std::is_base_of_v<Container, ContainerType>, "ContainerType must derive from Container");
-
-        using ContainerType::ContainerType;
-
-        void render(sf::RenderWindow& rw) override {
-            BoxElement::render(rw);
-            ContainerType::render(rw);
-        }
-    };
-
 } // namespace ofc::ui::dom
